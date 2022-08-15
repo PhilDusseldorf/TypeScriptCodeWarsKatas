11fd9c9b2a5a2f1a45f4d207fb7d1914c545843b
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2022-08-15 09:08:34 +0200

    :white_check_mark: updated tests duplicateEncoder

--- src/katas/duplicateEncoder.test.ts
+++ src/katas/duplicateEncoder.test.ts
@@ -1,19 +1,16 @@
 import { assert } from 'chai';
 import { duplicateEncoder } from './duplicateEncoder';
 
-// TODO Add your tests here
 describe('example', () => {
   it('test', () => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    assert.strictEqual(duplicateEncoder('din'), 'din');
-
-    // assert.strictEqual(duplicateEncoder('din'), '(((');
-    // assert.strictEqual(duplicateEncoder('recede'), '()()()');
-    // assert.strictEqual(
-    //   duplicateEncoder('Success'),
-    //   ')())())',
-    //   'should ignore case',
-    // );
-    // assert.strictEqual(duplicateEncoder('(( @'), '))((');
+    assert.strictEqual(duplicateEncoder('din'), '(((');
+    assert.strictEqual(duplicateEncoder('recede'), '()()()');
+    assert.strictEqual(
+      duplicateEncoder('Success'),
+      ')())())',
+      'should ignore case',
+    );
+    assert.strictEqual(duplicateEncoder('(( @'), '))((');
   });
 });
