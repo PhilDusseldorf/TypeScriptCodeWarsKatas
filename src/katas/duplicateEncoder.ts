1a0708c8e85ea23562d9fb3fef4e9bffe256125e
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2022-08-15 09:06:10 +0200

    finish duplicateEncoder

--- src/katas/duplicateEncoder.ts
+++ src/katas/duplicateEncoder.ts
@@ -1,3 +1,14 @@
-export function duplicateEncoder(word: string): string {
-  return word;
+export function duplicateEncoder(word: string): string{
+  let charArray = word.toLowerCase().split("");
+  let charPool = new Set<string>();
+  let duplicates = new Set<string>();
+  let result: string[] = []
+  
+  charArray.forEach((char: string) => {
+    charPool.has(char) ? duplicates.add(char) : charPool.add(char);
+  });
+  charArray.forEach((char: string) => {
+    duplicates.has(char) ? result.push(")") : result.push("(");
+  });
+  return result.join("");
 }
